c7845e839cba2560ef7243f1b2da5cc918a797dc
Yogesh Bhati <yogesh.bhati@indiamart.com>
2022-10-09 12:59:34 +0530

    create class product and add 3 overloaded method product

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,25 @@ package com.driver;
 
 public class Main {
 
+
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(1,2);
+        p.product(3,4,5);
+    }
+
+}
+class Product{
+    public int product(int x, int y) {
+        return 0;
+    }
+
+    public int product(int x, int y, int z) {
+        return 0;
+    }
+    public double product(double x, double y) {
+        return 0;
+    }
+
+
 }
